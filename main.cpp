7883a7f4e36f539301375a1e32b693c2d70a5ab9
David T. Montoya <davidtovarmontoya@gmail.com>
2021-01-22 21:09:06 -0500

    start operators v2

--- main.cpp
+++ main.cpp
@@ -11,22 +11,46 @@
 #include <iostream>
 #include "Stack.h"
 using namespace std;
+void testmethod()
+{
+
+}
 
 int main()
 {
+    //remember that 'Element' class is very importan for linklist or stack (check Element.h and Element.tpp)
     //testing element template
+    //int elements
     Element<int> one(1, nullptr);
     Element<int> two(2, &one);
+    Element<int> tre(3, &two);
+    Element<int> four(4, &tre);
+    Element<int> five(5, &four);
+    Element<int> sex(6, &five);
+
+  //double elements
     Element<double> treeDec(3.44, nullptr );
     Element<double> fourDec(4.55, &treeDec );
+    Element<double> feveDec(5.55, &treeDec );
+    Element<double> sixDec(6.55, &treeDec );
+    Element<double> nineDec(9, &treeDec );
+//string elements
     Element<string> hello("Hi,", nullptr );
     Element<string> world("world", &hello );
+    Element<string> world1("world1", &world );
+    Element<string> world2("world2", &world1 );
+    Element<string> world3("world3", &world2 );
+    Element<string> world4("world5", &world3 );
+    Element<string> world5("world4", &world4 );
 
 
 
     Stack<int> pila;
 
-    cout<< "hla"<<endl<< two.valueNext() <<endl;
+    cout<< " Running function .valueNext of elemente 'two'"<<endl<<"Result: "<< two.valueNext() <<endl<<endl;
+    cout<< "Running function .valueNext of elemente 'world'"<<endl<<"Result: "<< world.valueNext() <<endl<<endl;
+    cout<< "Running function .valueNext of elemente fourDec"<<endl<<"Result: "<< fourDec.valueNext() <<endl<<endl;
+
     cout<< "prueba "<<"lenght de la pila: "<<  pila.len()<<endl;
 
     Stack<string> cadenas;
